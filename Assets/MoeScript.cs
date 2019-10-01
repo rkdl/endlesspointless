dff1a7e51477db2a865903ddcd07b1ca6cce32f2
Bogdan <bogdan.minzar@gmail.com>
2019-10-01 21:54:58 +0300

    add bartender and tyanka

---
+++ Assets/MoeScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoeScript : MonoBehaviour
+{
+    private Animator animator;
+
+    private static readonly int PlayIdle = Animator.StringToHash("playIdle");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0f, 1f) < 0.0025f)
+        {
+            animator.SetTrigger(PlayIdle);
+        }
+    }
+}
