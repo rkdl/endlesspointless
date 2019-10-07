3d86b28c67829fd39437ec53d55a2857744f0034
Bogdan <bogdan.minzar@gmail.com>
2019-10-07 17:35:36 +0300

    WIP infinite scroll & parallax

---
+++ Assets/BackgroundController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    public float moveMultiplier = 0.25f;
+
+    private readonly Vector2 backgroundMoveDirection = new Vector2(1, -1);
+    private Vector2 previousCameraPosition;
+    private Camera mainCamera;
+    private Sprite sprite;
+    
+    private Vector2 screenBounds;
+    void Start()
+    {
+        mainCamera = Camera.main;
+        sprite = GetComponent<Sprite>();
+        previousCameraPosition = mainCamera.transform.position;
+    }
+
+    void Update()
+    {
+        Vector2 currentCameraPosition = mainCamera.transform.position;
+
+        var backgroundTranslation =
+            (currentCameraPosition - previousCameraPosition) *
+            backgroundMoveDirection * 
+            moveMultiplier;
+        transform.Translate(backgroundTranslation);
+
+        var halfCamWidth = mainCamera.aspect * mainCamera.orthographicSize;
+        var camLeftXBound = currentCameraPosition.x - halfCamWidth;
+        var camRightXBound = currentCameraPosition.x + halfCamWidth;
+
+        previousCameraPosition = mainCamera.transform.position;
+        
+        var renderer = GetComponent<Renderer>();
+
+        var spriteLeftXBound = renderer.transform.position.x - renderer.bounds.extents.x;
+        var spriteRightXBound = renderer.transform.position.x + renderer.bounds.extents.x;
+        var delta = 0.1f;
+        if (camRightXBound + delta > spriteRightXBound)
+        {
+            // need to spawn texture right
+        }
+        else if (camLeftXBound - delta < spriteLeftXBound)
+        {
+            // spawn texture left
+        }
+    }
+}
